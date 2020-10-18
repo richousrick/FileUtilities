2c9a2afe3457ed44baef6fdf0e06671729b21850
Rikkey <rikkeypaal@gmail.com>
2020-10-18 16:31:55 +0100

    Implemented property handling

---
+++ src/main/scala/richousrick/fileutilities/lib/PropertiesManager.scala
@@ -0,0 +1,56 @@
+package richousrick.fileutilities.lib
+
+import java.nio.channels.{Channels, FileChannel}
+import java.nio.file.{Path, StandardOpenOption}
+import java.util.Properties
+
+import scala.util.{Failure, Success, Using}
+
+/**
+ * Library enabling simple properties IO
+ */
+object PropertiesManager {
+
+	/**
+	 * Writes the properties to the specified config file
+	 *
+	 * @param configFile file to write the configuration to
+	 * @param prop       properties to write
+	 * @return true if the properties were successfully written
+	 */
+	def writeConfig(configFile: Path, prop: Properties): Boolean =
+		Using.Manager { use =>
+			val channel = use(FileChannel.open(configFile, StandardOpenOption.WRITE, StandardOpenOption.CREATE))
+			use(channel.lock())
+
+			prop.store(Channels.newOutputStream(channel), null)
+		} match {
+			case Failure(exception) =>
+				System.err.println(s"Could not create config file: $exception")
+				false
+
+			case Success(_) => true
+		}
+
+	/**
+	 * Reads properties from a config file
+	 *
+	 * @param configFile to read the properties file from
+	 * @return Some(properties) if successful otherwise None
+	 */
+	def readConfig(configFile: Path): Option[Properties] =
+		Using.Manager { use =>
+			val channel = use(FileChannel.open(configFile, StandardOpenOption.READ))
+			use(channel.lock(0L, Long.MaxValue, true))
+
+			val prop = new Properties()
+			prop.load(Channels.newInputStream(channel))
+			prop
+		} match {
+			case Failure(exception) =>
+				System.err.println(s"Could not read config file: $exception")
+				None
+
+			case Success(properties) => Some(properties)
+		}
+}
---
+++ src/test/scala/richousrick/fileutilities/lib/PropertiesManagerTest.scala
@@ -0,0 +1,54 @@
+package richousrick.fileutilities.lib
+
+import java.nio.file.{FileSystem, Files, Path}
+import java.util.Properties
+
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.funsuite.AnyFunSuite
+
+class PropertiesManagerTest extends AnyFunSuite with BeforeAndAfterEach {
+	var fs: FileSystem = _
+	var targetFile: Path = _
+	var backupDir: Path = _
+	var propLoc: Path = _
+	var prop: Properties = _
+
+	override def beforeEach() {
+		// Gen mock file system containing target file
+		MockUtils.generateMockFilesystemWin() match {
+			case (a, b, c) =>
+				fs = a
+				targetFile = b
+				backupDir = c
+			case _ =>
+		}
+		propLoc = fs.getPath(backupDir + "\\versions.properties")
+
+		// generate default config and write it to the target directory
+		prop = new Properties()
+		prop.setProperty("Lorem", "Ipsum")
+		prop.setProperty("dolor", "sit ")
+		PropertiesManager.writeConfig(propLoc, prop)
+	}
+
+	test("Create and read new config file") {
+		// test the config file was created, and when loaded is identical to the
+		assert(Files.exists(propLoc))
+		val loadedProps = PropertiesManager.readConfig(propLoc)
+		assert(loadedProps.isDefined)
+		assert(loadedProps.get == prop)
+	}
+
+	test("Overwrite and read config file") {
+		// assert current props match stored props
+		assert(PropertiesManager.readConfig(propLoc).get == prop)
+
+		// change property and verify differs from stored props
+		prop.setProperty("otherProperty", "SomeValue")
+		assert(PropertiesManager.readConfig(propLoc).get != prop)
+
+		// rewrite and verify they match again
+		PropertiesManager.writeConfig(propLoc, prop)
+		assert(PropertiesManager.readConfig(propLoc).get == prop)
+	}
+}
