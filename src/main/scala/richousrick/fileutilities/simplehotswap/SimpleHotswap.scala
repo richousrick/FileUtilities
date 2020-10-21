ccc6326d0334bdf9a097c76cdaea4b024056c0a3
Rikkey <rikkeypaal@gmail.com>
2020-10-21 14:20:01 +0100

    Extended initLink to handle hard links

--- src/main/scala/richousrick/fileutilities/simplehotswap/SimpleHotswap.scala
+++ src/main/scala/richousrick/fileutilities/simplehotswap/SimpleHotswap.scala
@@ -24,13 +24,19 @@ object SimpleHotswap {
 	}
 
 	/**
-	 * Moves the backup file to the backup folder and creates a hard link in its place referring to its new position
+	 * Moves the backup file to the backup folder and creates a link in its place referring to its new position
 	 *
 	 * @param backupFolder location to store the current instance
-	 * @param targetFile   file to be moved and replaced with a hard link
+	 * @param targetFile   file to be moved and replaced with a link
+	 * @param hard         if true the link will be hard, otherwise it will be symbolic
 	 * @return true if successful
 	 */
-	def initLink(backupFolder: Path, targetFile: Path): Boolean = {
+	def initLink(backupFolder: Path, targetFile: Path, hard: Boolean): Boolean = {
+		if (hard && Files.isDirectory(targetFile)) {
+			System.err.println(s"Hard links are unsupported for directories")
+			return false
+		}
+
 		val backupInstance = backupFolder.resolve("currentVersionInstance.current")
 		try {
 			Files.move(targetFile, backupInstance)
@@ -41,7 +47,10 @@ object SimpleHotswap {
 		}
 
 		try {
-			Files.createSymbolicLink(targetFile, backupInstance)
+			if (hard)
+				Files.createLink(targetFile, backupInstance)
+			else
+				Files.createSymbolicLink(targetFile, backupInstance)
 			true
 		} catch {
 			case e@(_: IOException | _: SecurityException) =>
--- src/test/scala/richousrick/fileutilities/simplehotswap/SimpleHotswapTest.scala
+++ src/test/scala/richousrick/fileutilities/simplehotswap/SimpleHotswapTest.scala
@@ -1,11 +1,13 @@
 package richousrick.fileutilities.simplehotswap
 
-import java.nio.file.Files
+import java.nio.channels.{Channels, FileChannel}
+import java.nio.file.{Files, StandardOpenOption}
 
 import org.scalatest.funsuite.AnyFunSuite
 import richousrick.fileutilities.lib.MockUtils
 
 import scala.jdk.CollectionConverters._
+import scala.util.Using
 
 class SimpleHotswapTest extends AnyFunSuite {
 
@@ -34,7 +36,7 @@ class SimpleHotswapTest extends AnyFunSuite {
 
 		// assert filesystem is empty before backup
 		assert(Files.list(backupDir).count() == 0)
-		SimpleHotswap.initLink(backupDir, targetFile)
+		SimpleHotswap.initLink(backupDir, targetFile, hard = false)
 
 		// assert the file was successfully copied to the backup folder
 		assert(Files.list(backupDir).count() == 1)
@@ -57,7 +59,7 @@ class SimpleHotswapTest extends AnyFunSuite {
 
 		// assert filesystem is empty before backup
 		assert(Files.list(backupDir).count() == 0)
-		SimpleHotswap.initLink(backupDir, targetFile)
+		SimpleHotswap.initLink(backupDir, targetFile, hard = false)
 
 		// assert the file was successfully copied to the backup folder
 		// Convert both to string instead of for easier readability in print
@@ -72,5 +74,44 @@ class SimpleHotswapTest extends AnyFunSuite {
 		// test link was created successfully
 		assert(Files.isSymbolicLink(targetFile))
 		assert(Files.readSymbolicLink(targetFile) == backupInstance)
+	}
+
+	test("Setup a hard linked backup file") {
+		val (fs, targetFile) = MockUtils.generateMockFilesystemWin()
+		val backupDir = fs.getPath("""C:\data\backup""")
+		val fileContents = Files.readAllLines(targetFile).asScala
+		val backupInstance = backupDir.resolve("currentVersionInstance.current")
+
+		// create backup folder
+		Files.createDirectories(backupDir)
+
+		// assert filesystem is empty before backup
+		assert(Files.list(backupDir).count() == 0)
+		SimpleHotswap.initLink(backupDir, targetFile, hard = true)
+
+		// assert the file was successfully copied to the backup folder
+		assert(Files.list(backupDir).count() == 1)
+		assert(Files.exists(backupInstance))
+		assert(Files.readAllLines(backupInstance).asScala == fileContents)
+
+		// test link was created successfully
+		// modify target file, test backup version changes
+		val original = Files.readAllBytes(backupInstance)
+		Using.Manager { use =>
+			val channel = use(FileChannel.open(targetFile, StandardOpenOption.APPEND))
+			use(channel.lock)
+			val out = use(Channels.newOutputStream(channel))
+			out.write("Hello".getBytes)
+		}
+
+		assert(Files.readAllBytes(backupInstance) != original)
+		assert(Files.readAllBytes(targetFile).toSeq == Files.readAllBytes(backupInstance).toSeq)
+	}
+
+	test("Setup a hard linked backup directory") {
+		val (fs, targetFile, filesToBackup) = MockUtils.generateMockFilesystemWinDir()
+		val backupDir = fs.getPath("""C:\data\backup""")
+
+		assert(!SimpleHotswap.initLink(backupDir, targetFile, hard = true))
 	}
 }
