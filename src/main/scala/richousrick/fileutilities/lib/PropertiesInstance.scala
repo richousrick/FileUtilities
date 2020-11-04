b51b3d1c3a4f433fb19962962cdbedf2c7e7b606
Rikkey <rikkeypaal@gmail.com>
2020-11-04 12:56:57 +0000

    Implemented loading TypedProperty at runtime

--- src/main/scala/richousrick/fileutilities/lib/PropertiesInstance.scala
+++ src/main/scala/richousrick/fileutilities/lib/PropertiesInstance.scala
@@ -1,12 +1,42 @@
 package richousrick.fileutilities.lib
 
-import scala.reflect.runtime.universe._
+import scala.reflect.runtime.universe.{TypeTag, typeOf}
 
 /**
  * Companion object for [[richousrick.fileutilities.lib.PropertiesInstance]].
  */
 object PropertiesInstance {
 
+	/**
+	 * Gets an instance of [[richousrick.fileutilities.lib.TypedProperty TypedProperty]] that can handle a desired datatype.
+	 *
+	 * @param tt type tag of the desired datatype
+	 * @tparam T desired datatype
+	 * @return a [[richousrick.fileutilities.lib.TypedProperty TypedProperty]] that can handle data T, or None if T is an unsupported datatype
+	 */
+	private def resolveHandler[T](implicit tt: TypeTag[T]): Option[TypedProperty[T]] = (tt.tpe match {
+		case e if e <:< typeOf[Enumeration] =>
+			// Get base class and cast to a subclass of Enumeration.
+			// Then pass that class to a function that will build an instance
+			// Note: 	Type checking fails when extracting the type from the class
+			// 				So a function is used that extracts the type from the class and uses that
+			def makeInstance[E <: Enumeration : TypeTag](c: Class[E]): EnumProperty[E] = EnumProperty[E]
+
+			makeInstance(tt.mirror.runtimeClass(tt.tpe).asSubclass[Enumeration](classOf[Enumeration]))
+		case s if s =:= typeOf[String] => StringProperty
+		case c if c =:= typeOf[Char] => CharProperty
+		case b if b =:= typeOf[Boolean] => BooleanProperty
+		case b if b =:= typeOf[Byte] => ByteProperty
+		case s if s =:= typeOf[Short] => ShortProperty
+		case i if i =:= typeOf[Int] => IntProperty
+		case l if l =:= typeOf[Long] => LongProperty
+		case f if f =:= typeOf[Float] => FloatProperty
+		case d if d =:= typeOf[Double] => DoubleProperty
+		case _ => null
+	}) match {
+		case null => None
+		case tp => Some(tp.asInstanceOf[TypedProperty[T]])
+	}
 }
 
 /**
---
+++ src/test/scala/richousrick/fileutilities/lib/PropertiesInstanceTest.scala
@@ -0,0 +1,54 @@
+package richousrick.fileutilities.lib
+
+import java.nio.file.Path
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.{PrivateMethodTester, Suites}
+import richousrick.fileutilities.simplehotswap.SimpleHotswap.LinkType
+
+import scala.math.BigDecimal.RoundingMode
+import scala.reflect.runtime.universe.TypeTag
+
+class PropertiesInstanceTestSuite extends Suites {
+	new PropertiesInstanceResolverTest
+}
+
+
+class PropertiesInstanceResolverTest extends AnyFunSuite with PrivateMethodTester {
+
+	test("resolveHandler resolves the correct primitive types") {
+		tryTest[String](StringProperty)
+		tryTest[Char](CharProperty)
+		tryTest[Byte](ByteProperty)
+		tryTest[Short](ShortProperty)
+		tryTest[Int](IntProperty)
+		tryTest[Long](LongProperty)
+		tryTest[Float](FloatProperty)
+		tryTest[Double](DoubleProperty)
+	}
+
+	test("resolverHandler resolves enum types") {
+		val rm = resolveHandler[RoundingMode.type]
+		assert(rm.isDefined)
+		assert(rm.get.isInstanceOf[EnumProperty[RoundingMode.type]])
+
+		val lt = resolveHandler[LinkType.type]
+		assert(lt.isDefined)
+		assert(lt.get.isInstanceOf[EnumProperty[LinkType.type]])
+	}
+
+	test("resolverHandler fails safe") {
+		assert(resolveHandler[Path].isEmpty)
+		assert(resolveHandler[List[String]].isEmpty)
+	}
+
+	def tryTest[T: TypeTag](expected: TypedProperty[_]): Unit = {
+		assert(resolveHandler[T].contains(expected))
+	}
+
+	def resolveHandler[T: TypeTag]: Option[TypedProperty[T]] = {
+		val handle = PrivateMethod[Option[TypedProperty[T]]](Symbol("resolveHandler"))
+		PropertiesInstance invokePrivate handle(implicitly[TypeTag[T]])
+	}
+}
+
